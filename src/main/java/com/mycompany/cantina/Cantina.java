d56cdcfd5aca52c3b834e7f7311e6d2225eeced2
giordani-francelino <francelino.giordani@gmail.com>
2023-06-16 16:16:53 -0300

    inicializcao

---
+++ src/main/java/com/mycompany/cantina/Cantina.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.cantina;
+
+/**
+ *
+ * @author Caixa
+ */
+public class Cantina {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ src/main/java/com/mycompany/cantina/repositorio/Dao.java
@@ -0,0 +1,184 @@
+/*
+ * CC BY-NC-SA 4.0
+ */
+package com.mycompany.cantina.repositorio;
+
+
+import com.mycompany.cantina.entidade.Entidade;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ * Class Dao
+ *
+ * @author Luis Guisso &lt;luis dot guisso at ifnmg dot edu dot br&gt;
+ * @version 0.1, 2022-10-24
+ * @param <T> Entidade data type
+ */
+public abstract class Dao<T>
+        implements IDao<T> {
+
+    public static final String DB = "cantina";
+
+    @Override
+    public Long saveOrUpdate(T e) {
+
+        // Primary key
+        Long id = 0L;
+
+        if (((Entidade) e).getId() == null
+                || ((Entidade) e).getId() == 0) {
+
+            // Insert a new register
+            // try-with-resources
+            try ( PreparedStatement preparedStatement
+                    = DbConnection.getConnection().prepareStatement(
+                            getSaveStatment(),
+                            Statement.RETURN_GENERATED_KEYS)) {
+
+                // Assemble the SQL statement with the data (->?)
+                composeSaveOrUpdateStatement(preparedStatement, e);
+
+                // Show the full sentence
+                System.out.println(">> SQL: " + preparedStatement);
+
+                // Performs insertion into the database
+                preparedStatement.executeUpdate();
+
+                // Retrieve the generated primary key
+                ResultSet resultSet = preparedStatement.getGeneratedKeys();
+
+                // Moves to first retrieved data
+                if (resultSet.next()) {
+
+                    // Retrieve the returned primary key
+                    id = resultSet.getLong(1);
+                }
+
+            } catch (Exception ex) {
+                System.out.println(">> " + ex);
+            }
+
+        } else {
+            // Update existing record
+            try ( PreparedStatement preparedStatement
+                    = DbConnection.getConnection().prepareStatement(
+                            getUpdateStatment())) {
+
+                // Assemble the SQL statement with the data (->?)
+                composeSaveOrUpdateStatement(preparedStatement, e);
+
+                // Show the full sentence
+                System.out.println(">> SQL: " + preparedStatement);
+
+                // Performs the update on the database
+                preparedStatement.executeUpdate();
+
+                // Keep the primary key
+                id = ((Entidade) e).getId();
+
+            } catch (Exception ex) {
+                System.out.println("Exception: " + ex);
+            }
+        }
+
+        return id;
+    }
+
+    @Override
+    public T findById(Long id) {
+
+        try ( PreparedStatement preparedStatement
+                = DbConnection.getConnection().prepareStatement(
+                        getFindByIdStatment())) {
+
+            // Assemble the SQL statement with the id
+            preparedStatement.setLong(1, id);
+
+            // Show the full sentence
+            System.out.println(">> SQL: " + preparedStatement);
+            // Performs the query on the database
+            ResultSet resultSet = preparedStatement.executeQuery();
+
+            // Returns the respective object if exists
+            if (resultSet.next()) {
+                return extractObject(resultSet);
+            }
+
+        } catch (Exception ex) {
+            System.out.println("Exception: " + ex);
+        }
+
+        return null;
+    }
+
+
+    @Override
+    public boolean deleteById(Long id) {
+
+        try ( PreparedStatement preparedStatement
+                = DbConnection.getConnection().prepareStatement(
+                        getDeleteByIdStatment())) {
+
+            // Assemble the SQL statement with the id
+            preparedStatement.setLong(1, id);
+
+            // Show the full sentence
+            System.out.println(">> SQL: " + preparedStatement);
+            // Performs the query on the database
+
+            // Returns the respective object if exists
+                return preparedStatement.execute();
+
+        } catch (Exception ex) {
+            System.out.println("Exception: " + ex);
+        }
+
+        return false;
+    }
+
+    @Override
+    public List<T> findAll() {
+
+        try ( PreparedStatement preparedStatement
+                = DbConnection.getConnection().prepareStatement(
+                        getFindAllStatment())) {
+
+            // Show the full sentence
+            System.out.println(">> SQL: " + preparedStatement);
+
+            // Performs the query on the database
+            ResultSet resultSet = preparedStatement.executeQuery();
+
+            // Returns the respective object
+            return extractObjects(resultSet);
+
+        } catch (Exception ex) {
+            System.out.println("Exception: " + ex);
+        }
+
+        return null;
+    }
+
+    @Override
+    public List<T> extractObjects(ResultSet resultSet) {
+        List<T> objects = new ArrayList<>();
+
+        try {
+            while (resultSet.next()) {
+                objects.add(extractObject(resultSet));
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(Dao.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+        return objects.isEmpty() ? null : objects;
+    }
+
+}
